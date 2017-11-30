0f6e707ac8fecdccd86abbe0a67fdb7431ac2ab4
Rostislav Raykov <rostislav@technocreatives.com>
2017-11-30 21:52:42 +0100

    feat: Add some simple symlinking

---
+++ symlink.sh
@@ -0,0 +1,31 @@
+DOTFILES=$DOTFILES
+
+# ZSH
+if [ -e ~/.zshrc ]; then
+	echo ".zshrc already exists"
+else
+	ln -s $DOTFILES/.zshrc ~/.zshrc
+fi
+
+# GIT
+if [ -e ~/.gitconfig ]; then
+        echo ".gitconfig already exists"
+else
+        ln -s $DOTFILES/.gitconfig ~/.gitconfig
+fi
+
+# VSCode
+if [ ! -d ~/Library/Application\ Support/Code/User ]; then
+	echo "VSCode settings folder does not exist. Check if you installed VSCode and it has been launched at least once."
+else
+	if [ ! -e ~/Library/Application\ Support/Code/User/settings.json ]; then
+		ln -s $DOTFILES/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+	else
+		echo "VSCode settings.json exists. Remove it and rerun."
+	fi
+	if [ ! -d ~/Library/Application\ Support/Code/User/snippets ]; then
+		ln -s $DOTFILES/vscode/snippets/ ~/Library/Application\ Support/Code/User/snippets
+	else
+		echo "VSCode snippets folder exists. Remove it and rerun."
+	fi
+fi
