02bab5f4d302f15135b9a270a6a67cea34ba22a5
Rostislav Raykov <z@zbrox.org>
2020-04-23 10:55:10 +0200

    add a check for staged files to versionbump function

--- fish/functions/versionbump.fish
+++ fish/functions/versionbump.fish
@@ -3,6 +3,12 @@
 
 function versionbump --description "Create a commit when just incrementing versions on a release/ branch"
     set -l VERSION (git symbolic-ref HEAD | cut -d'/' -f4)
+    set -l STAGED_FILES (git diff --name-only --cached)
+
+    if test -z $STAGED_FILES
+        echo "There are no staged changes. Are you sure you're done?"
+        return 1
+    end
 
     if test -z $VERSION
         echo "You're not on a version branch"
