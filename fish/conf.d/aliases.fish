acfc4a706a077d85415f7a93d000bc21191bb60f
Rostislav Raykov <z@zbrox.org>
2020-04-23 14:09:47 +0200

    turn some aliases to abbreviations

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -10,15 +10,15 @@ alias l 'exa --all --long --modified --group --header --color-scale'
 # navigating & such
 abbr -a o 'open'
 abbr -a mkdir 'mkdir -pv' # create folders along the path always
-alias search 'rg -i'
+abbr -a search 'rg -i'
 
 # git
-alias c 'git commit -m'
-alias commit 'git commit -m'
-alias master 'git checkout master'
-alias g 'gitup'
-alias amend 'git commit --amend --no-edit'
-alias msgamend 'git commit --amend -m'
+abbr -a c 'git commit -m'
+abbr -a commit 'git commit -m'
+abbr -a master 'git checkout master'
+abbr -a g 'gitup'
+abbr -a amend 'git commit --amend --no-edit'
+abbr -a msgamend 'git commit --amend -m'
 
 # ssh
 alias copyssh 'pbcopy < $HOME/.ssh/id_rsa.pub'
