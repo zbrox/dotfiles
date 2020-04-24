a1e78aa89c0cf7edf87001f73da2548f12050a24
Rostislav Raykov <z@zbrox.org>
2020-04-24 20:12:01 +0200

    some more fun with welcome messages

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,7 +1,12 @@
 # This overwrites the welcome message shown on session init
 function fish_greeting
-    set -l GREETINGS "Само спокойно!"
+    set GREETINGS "Само спокойно!"
     set -a GREETINGS "DON'T PANIC!"
+    set -a GREETINGS "Не панікуй!"
+    set -a GREETINGS "Астанавитесь!"
+    set -a GREETINGS "Få inte panik!"
+    set -a GREETINGS "Knock knock"
+    set -a GREETINGS "Look at how fast this was!"
 
-    echo (random choice $GREETINGS)
+    printf (set_color red)"%s\n" (random choice $GREETINGS)
 end
