3819c40ce5c3020a8bfd07afb82e17f04bcd0651
Rostislav Raykov <z@zbrox.org>
2020-05-04 18:36:01 +0200

    fix isodatetime function to return time offset in datetimeformat withe colon in it

--- fish/functions/isodatetime.fish
+++ fish/functions/isodatetime.fish
@@ -1,4 +1,7 @@
 function isodatetime
     # 2020-12-12T11:13:00+01:00
-    date +%Y-%m-%d\T%H:%M:%S%z
+    set -l TIME_OFFSET (date +%z)
+    set -l TIME_OFFSET_HOUR (string sub -s 1 -l 3 "$TIME_OFFSET")
+    set -l TIME_OFFSET_MIN (string sub -s 4 -l 2 "$TIME_OFFSET")
+    echo (date +%Y-%m-%d\T%H:%M:%S)$TIME_OFFSET_HOUR":"$TIME_OFFSET_MIN
 end
