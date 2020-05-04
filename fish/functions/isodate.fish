70f6b397ace407ebd709a1ba35e54f1c995c13cc
Rostislav Raykov <z@zbrox.org>
2020-05-04 18:29:04 +0200

    add short isodate variant to function

--- fish/functions/isodate.fish
+++ fish/functions/isodate.fish
@@ -1,6 +1,11 @@
 # source: https://github.com/razzius/fish-functions/blob/master/functions/isodate.fish
 # returns date in the ISO 8601 format without time
 
-function isodate
-    date +%Y-%m-%d
+function isodate --argument short
+    set -q short or set short "false"
+    if test -z $short
+        date +%Y-%m-%d
+    else
+        date +%Y%m%d
+    end
 end
