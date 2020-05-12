222cffcecf1b73fb052800cbdf4e171a4dc128be
Rostislav Raykov <z@zbrox.org>
2020-05-12 11:01:52 +0200

    fix a bug in dorelease function

--- fish/functions/dorelease.fish
+++ fish/functions/dorelease.fish
@@ -3,9 +3,9 @@
 
 function dorelease --description "Helper for making releases git-flow style"
     set -l BRANCH (git symbolic-ref HEAD | cut -d'/' -f3)
-    if string match "$BRANCH" "release*"
+    if string match "$BRANCH" "release"
         set -l VERSION (git symbolic-ref HEAD | cut -d'/' -f4)
-        git flow release finish $VERSION
+        git flow release finish "$VERSION"
     else
         set -l VERSION $argv[1]
         if test -z "$VERSION"
