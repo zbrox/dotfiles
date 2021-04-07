37011e9e8d1f1ef9c21641915fa8270aa2a6d173
Rostislav Raykov <me@zbrox.com>
2021-04-07 14:05:26 +0200

    forgotten semicolon in sync-fork alias

--- fish/functions/sync-fork.fish
+++ fish/functions/sync-fork.fish
@@ -1,5 +1,5 @@
 function sync-fork --argument branch --description "Sync a branch (default is master) with the upstream remote"
-    set -q branch or set branch "master"
+    set -q branch[1]; or set branch "master"
     git fetch upstream
     git checkout $branch
     git rebase upstream/$branch
