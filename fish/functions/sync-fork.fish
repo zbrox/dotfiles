28c8ab0ed26ac41811bb9aafc7e6d045508ce8ad
Rostislav Raykov <me@zbrox.com>
2021-04-07 14:05:37 +0200

    add some echos in sync-fork

--- fish/functions/sync-fork.fish
+++ fish/functions/sync-fork.fish
@@ -1,6 +1,9 @@
 function sync-fork --argument branch --description "Sync a branch (default is master) with the upstream remote"
     set -q branch[1]; or set branch "master"
+    echo "Fetching upstream..."
     git fetch upstream
+    echo "Checking out branch $branch"
     git checkout $branch
+    echo "Rebasing from upstream/$branch"
     git rebase upstream/$branch
 end
