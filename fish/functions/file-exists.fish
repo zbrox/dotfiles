43641719c74523b965448757c505763e15e2967d
Rostislav Raykov <me@zbrox.com>
2021-05-14 18:18:19 +0200

    clean up some fish plugins and old functions

--- fish/functions/file-exists.fish
+++
@@ -1,6 +0,0 @@
-# source: https://github.com/razzius/fish-functions/blob/master/functions/file-exists.fish
-# small utility alias function
-
-function file-exists --argument file
-    test -e $file
-end
--- fish/functions/kube-port.fish
+++
@@ -1,4 +0,0 @@
-function kube-port --argument service --argument ports --description "Shorter kubectl port-forward"
-    set -l ports (string split ' ' $ports)
-    kubectl port-forward "service/$service" $ports --address (lan-ip)
-end
