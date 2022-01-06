253ffcbc6182ce893cede56397b53e36b9282553
Rostislav Raykov <me@zbrox.com>
2022-01-06 15:48:38 +0100

    change how brew is installed
    things have changed since last time i installed brew

--- install.sh
+++ install.sh
@@ -9,7 +9,9 @@ sudo -v
 # Check for Homebrew and install if we don't have it
 if test ! "$(which brew)"; then
   echo "Installing Homebrew..."
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/zbrox/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 else
   echo "Homebrew already installed."
 fi
