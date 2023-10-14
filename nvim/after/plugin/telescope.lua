e462b82f10facde7b797786c16218a98ce02ed04
Rostislav Raykov <me@zbrox.com>
2023-10-14 22:39:21 +0200

    experiment with some more telescope key mappings

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -3,3 +3,7 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 -- find within git files (project find)
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+-- ripgrep within the project files
+vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
+-- search within open buffers
+vim.keymap.set('n', '<leader>tb', builtin.buffers, {})
