58e287bd517d473b1ceac7f64186f4ca9444357c
Rostislav Raykov <me@zbrox.com>
2023-10-15 19:43:21 +0200

    nvim: fix typo

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -4,6 +4,6 @@ vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "Find files in pr
 -- find within git files (project find)
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "Find files in git repo" })
 -- ripgrep within the project files
-vim.keymap.set('n', '<C-f>', builtin.live_grep, { desc = "Ripgre in project files" })
+vim.keymap.set('n', '<C-f>', builtin.live_grep, { desc = "Ripgrep in project files" })
 -- search within open buffers
 vim.keymap.set('n', '<leader>tb', builtin.buffers, { desc = "Show open buffers" })
