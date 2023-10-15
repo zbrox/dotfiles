b0cd8bec259dbf88db2fc3cae5c1184477d813e7
Rostislav Raykov <me@zbrox.com>
2023-10-15 19:24:12 +0200

    set some options for nvim like indentation and line numbers

---
+++ nvim/lua/zbrox/opts.lua
@@ -0,0 +1,13 @@
+-- hybrid line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- indentation related
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- visual aid for column 80
+vim.opt.colorcolumn = "80"
