521724237758e60697ee384cb2965d190c4b3ece
Rostislav Raykov <me@zbrox.com>
2024-02-29 11:50:13 +0100

    fix nvim-tree keymappings

--- nvim/lua/zbrox/plugins/nvim-tree.lua
+++ nvim/lua/zbrox/plugins/nvim-tree.lua
@@ -3,10 +3,22 @@ return
 {
     'kyazdani42/nvim-tree.lua',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    keys = {
-        { "n", "<C-b>", ':NvimTreeToggle<CR>', desc = "Show tree view" }
-    },
     config = function()
+        local function on_attach(bufnr)
+            local api = require "nvim-tree.api"
+
+            local function opts(desc)
+                return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+            end
+
+            -- default mappings
+            api.config.mappings.default_on_attach(bufnr)
+
+            -- custom mappings
+            vim.keymap.set('n', '<leader>b', '<cmd>NvimTreeToggle<CR>', { desc = "Show tree view" })
+            vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+        end
+
         require('nvim-tree').setup {
             disable_netrw = true,
             hijack_netrw = true,
@@ -16,7 +28,8 @@ return
             },
             filters = {
                 custom = { ".git" },
-            }
+            },
+            on_attach = on_attach,
         }
     end,
 }
