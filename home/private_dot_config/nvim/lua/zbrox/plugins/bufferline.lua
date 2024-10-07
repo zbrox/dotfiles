daaa17aa585227195a12ac4580da9df45bd290f6
Rostislav Raykov <me@zbrox.com>
2024-10-07 20:36:28 +0200

    nvim: away with tabs, only buffers

--- home/private_dot_config/nvim/lua/zbrox/plugins/bufferline.lua
+++ home/private_dot_config/nvim/lua/zbrox/plugins/bufferline.lua
@@ -1,10 +1,18 @@
 return {
-    "akinsho/bufferline.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    version = "*",
-    opts = {
-        options = {
-            mode = "tabs",
+  "akinsho/bufferline.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  version = "*",
+  opts = {
+    options = {
+      mode = "buffers",
+      offsets = {
+        {
+          filetype = "NvimTree",
+          text = "File Explorer",
+          highlight = "Directory",
+          separator = true,
         },
+      },
     },
+  },
 }
