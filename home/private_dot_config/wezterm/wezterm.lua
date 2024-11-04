ae937f5fdf2e85959fcf6973330386151bee4e70
Rostislav Raykov <me@zbrox.com>
2024-11-04 13:38:19 +0100

    wezterm: try out jetbrains mono as a font

--- home/private_dot_config/wezterm/wezterm.lua
+++ home/private_dot_config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font = wezterm.font("JetBrainsMono Nerd Font Propo")
 config.font_size = 14
 
 -- disable tab bar
