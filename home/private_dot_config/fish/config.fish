683f9b2261e20700245e9a68bc78353e53435f11
Rostislav Raykov <me@zbrox.com>
2026-03-19 11:59:13 +0100

    fish: start zellij automatically

--- home/private_dot_config/fish/config.fish
+++ home/private_dot_config/fish/config.fish
@@ -47,3 +47,15 @@ zellij setup --generate-completion fish | source
 
 # fish vi mode
 fish_vi_key_bindings
+
+# Unset the default fish greeting text which messes up Zellij
+set fish_greeting
+
+# Check if we're in an interactive shell
+if status is-interactive
+
+    # At this point, specify the Zellij config dir, so we can launch it manually if we want to
+    export ZELLIJ_CONFIG_DIR=$HOME/.config/zellij
+
+    eval (zellij setup --generate-auto-start fish | string collect)
+end
